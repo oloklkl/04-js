3070c0f3b0e830566f1645db3653b43a07b57e5a
seongju76 <sungju7694@naver.com>
2024-11-07 15:14:00 +0900

    11/7

--- js/05-구문.js
+++ js/05-구문.js
@@ -109,3 +109,94 @@ function header(type) {
 
 console.log(header('type1'));
 console.log(header('type2'));
+
+// if문으로 변경
+function header2(type) {
+    if(type === "type1") {
+        return "타입1 header 출력";
+    }
+    if(type === "type2") {
+        return "타입2 header 출력";
+    }
+    return "타입3 header 출력";
+}
+
+console.log(
+header2("type1"),
+header2("type2"),
+header2("type3"),
+);
+
+// 반복문
+// for문
+//  for (초기값; 조건; 증감) {실행문
+
+// 콘솔창에 0부터 9까지 출력
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// 9부터 0까지 출력
+for(let i = 9; i > -1; i -=2) {
+    if(i < 4) {break;}
+    console.log(i);
+}
+
+// continue문
+// 9부터 1까지 출력
+for(let i = 9; i > 0; i--) {
+    if(i % 2 === 0) {
+        continue;}
+    console.log(`i : ${i}`);
+}
+
+// for of문 (배열)
+const animals = ['dog', 'cat', 'panda']
+
+for(let animal of animals) {
+    console.log('내가 좋아하는 동물은? ${animal}');
+}
+
+// for in문 (객체)
+const user3 = {
+    name: '겨울',
+    age: 2,
+    city: 'seoul'
+};
+
+for(let key in user3) {
+    console.log(key);
+    console.log(user3[key]);
+}
+
+// while문
+// while (조건) {실행문}
+let i = 1;
+let sum = 0;
+
+while(i <= 10) {
+    sum += i;    // sum = sum + i;
+    i++;
+}
+
+console.log(`sum : ${sum}, i : ${i}`);
+
+// i=1: sum = 0+1 = 1
+// i=2: sum = 1+2 = 3
+// i=3: sum = 3+3 = 6
+// i=4: sum = 6+4 = 10
+// i=5: sum = 10+5 = 15
+// i=6: sum = 15+6 = 21
+// i=7: sum = 21+7 = 28
+// i=8: sum = 28+8 = 36
+// i=9: sum = 36+9 = 45
+// i=10: sum = 45+10 = 55
+
+
+// do while문
+
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 10)
---
+++ js/06-함수.js
@@ -0,0 +1,107 @@
+// 함수
+// 1. 함수 선언하기
+// 함수 선언식
+
+hello1();   // 함수 호출
+
+function hello1() {
+    console.log("hello1");
+}
+
+// 함수 표현식
+const hello2 = function() {
+    console.log("hello2");
+}
+
+
+hello2();   // 함수 호출
+
+
+// return
+// 함수를 호출한 곳으로 값을 반환 및 종료
+
+function h1() {
+    return "hello";
+    console.log("world");   // 실행되지 않음
+}
+console.log(h1());
+
+
+function h2() {
+    return;
+}
+console.log(h2());
+
+function h3() {
+    console.log("hello");
+    return 1;
+    return 2;   // 실행되지 않음
+}
+console.log(h3());
+
+// 매개변수
+function sum(x = 0, y = 0) {
+    return x + y;
+}
+console.log(sum(1, 2));
+console.log(sum(5));
+
+// 함수의 구조 분해
+// 객체 분해
+const user = {
+    name: "윈터",
+    age: 24,
+};
+function getName({name, age, email = "이메일이 없습니다."}) {
+    // const {name, age} = props;
+    console.log(name);
+    // 윈터는 24살 입니다.
+    console.log(`${name}는 ${age}살 입니다.`);
+    console.log(email);
+}
+getName(user);
+
+// 배열 분해
+const animals = ['dog', , 'panda'];
+function getSecond([, b = "없음"]) {
+
+    // const arr = ['dog', 'cat', 'panda'];
+    // const [, b] = arr;
+
+
+    console.log(b);
+}
+getSecond(animals);
+
+// 나머지 매개변수
+function print(...num) {
+    console.log(num);
+}
+print(1, 2, 3, 4 , 5);
+
+// 화살표 함수
+// function hello() {};
+// hello();
+
+// const hello3 = function() {};
+// hello3();
+
+const hello4 = () => {
+    console.log("hello4");
+};
+hello4();
+
+// 화살표 함수의 축약
+const a = () => {};
+const b = x => {};
+const c = (x, y) => {};
+const d = (x, y) =>  x + y ;
+const e = (x, y) => {
+    console.log("hello");
+    return x + y;
+}
+const f = () => ({name : "윈터"});
+const g = () => [1, 2, 3];
+const h = () => {}
+const i = () => {}
+const j = () => {}
